e1a0aa6054a816b751dfbb4b55f06bf03c88f18f
farah25T <farah.toulgui@insat.ucar.tn>
2023-05-01 14:18:18 +0100

    log out logic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $session = $request->getSession();
+        $user = null;
+        $id = $session->get('id');
+        if (isset($id)) {
+            $user = $entityManager->getRepository(User::class)->find($id);
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+             'user' => $user
         ]);
     }
+    
+  
 }
