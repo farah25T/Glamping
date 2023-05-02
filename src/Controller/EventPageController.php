f3cd563e18437ad6e37d2abc00b3e778aee8b6a1
Donia <doniabougrine7@gmail.com>
2023-05-02 16:24:59 +0100

    event page front (1st)

---
+++ src/Controller/EventPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventPageController extends AbstractController
+{
+    #[Route('/event/page', name: 'app_event_page')]
+    public function index(): Response
+    {
+        return $this->render('event_page/index.html.twig', [
+            'controller_name' => 'EventPageController',
+        ]);
+    }
+}
