b689ece5c1bcb783ec55c23a73feb3f33ad542d6
nour <soussia.nour@inasta.ucar.tn>
2023-05-03 15:56:52 +0100

    home page first commit

---
+++ public/assets/js/homepage/map.js
@@ -0,0 +1,11 @@
+var map = L.map("map").setView([36.8002068, 10.1857757], 7);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([36.8002068, 10.1857757])
+  .addTo(map)
+ 
+  .openPopup();
