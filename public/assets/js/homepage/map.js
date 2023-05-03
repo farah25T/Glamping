909a59a9b3861d89b75da5b7c1ed9b000b022ef7
nour <soussia.nour@inasta.ucar.tn>
2023-05-03 19:20:13 +0100

    home_2

--- public/assets/js/homepage/map.js
+++ public/assets/js/homepage/map.js
@@ -5,7 +5,8 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([36.8002068, 10.1857757])
-  .addTo(map)
- 
-  .openPopup();
+L.marker([36.8002068, 10.1857757]).addTo(map) .openPopup();
+L.marker([36.206, 10.1857757]) .addTo(map) .openPopup();
+L.marker([36.206, 9.1857757]) .addTo(map) .openPopup();
+L.marker([36.5, 10.5857757]) .addTo(map) .openPopup();
+
