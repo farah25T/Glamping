1279d2fca377e656d3bf35957cf8ed6885f1a6a5
nour <soussia.nour@inasta.ucar.tn>
2023-05-05 13:13:45 +0100

    pre_event_user_page

---
+++ src/Controller/EventUserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use function Webmozart\Assert\Tests\StaticAnalysis\string;
+use Symfony\Component\Security\Core\Security;
+class EventUserController extends AbstractController
+{
+
+    #[Route('/event/user', name: 'app_event_user')]
+    public function index(EntityManagerInterface $entityManager , SessionInterface $session): Response
+    {
+
+
+        $userIsFound = $entityManager->getRepository(User::class)->findOneById($session->get('id'));
+        return $this->render('event_user/index.html.twig', [
+            'user_name'=>$userIsFound->getname(),
+        ]);
+    }
+}
