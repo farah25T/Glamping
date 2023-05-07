2f9215b6ff40c64c9495ddffc6984ab80ff9585b
addala <addala.raed@gmail.com>
2023-05-07 10:11:46 +0200

    correct mistakes

--- src/Controller/CataloguePageController.php
+++ src/Controller/CataloguePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
