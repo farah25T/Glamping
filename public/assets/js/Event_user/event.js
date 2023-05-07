7413205c18fb8266d3d6798ef0778819d3ac9c9d
nour <soussia.nour@inasta.ucar.tn>
2023-05-07 09:43:49 +0100

    event user page added the back of saved event

--- public/assets/js/Event_user/event.js
+++ public/assets/js/Event_user/event.js
@@ -10,22 +10,146 @@ const saved = document.querySelector('#saved');
 const saved_bouton = document.querySelector('#saved_bouton');
 const liked = document.querySelector('#liked');
 const liked_bouton = document.querySelector('#liked_bouton');
+const saved_card = document.querySelector('#hidden');
 
-saved_bouton.addEventListener('click', (event) => {
+
+
+
+console.log(saved_card)
+
+
+
+
+
+
+
+
+
+saved_bouton.addEventListener('click', function() {
+
+
+
     if (!liked.hasAttribute('hidden'))
         liked.toggleAttribute('hidden');
     if (!booked.hasAttribute('hidden'))
         booked.toggleAttribute('hidden');
-    saved.toggleAttribute('hidden');
+   if(saved.hasAttribute('hidden'))
+   {saved.removeAttribute('hidden');
+       fetch('/event_saved')
+           .then(response => response.json())
+           .then(response => {
+               p=0;
+               while( response[p])
+               {
+                   const card$p = saved_card.cloneNode(true);
+                   Event_name= card$p.querySelector('.descri')
+
+                   Event_name.innerHTML =response[p].name
+
+
+                   card$p.setAttribute('class','saved_card_eff' )
+                   console.log(card$p);
+                   card$p.removeAttribute('hidden')
+                   saved.appendChild(card$p);
+                   p+=1;
+               }
+
+
+           })
+           .catch(error => {
+
+                   console.error('Error:', error);
+               }
+           );
+   }
+   else
+   {
+       const temp_card = document.querySelectorAll('.saved_card_eff')
+       p=0
+    while(temp_card[p])
+    {
+        temp_card[p].remove();
+        p++;
+    }
+       saved.hidden=true;
+   }
+
+
 });
 
-liked_bouton.addEventListener('click', (event) => {
+
+
+liked_bouton.addEventListener('click', function() {
+
+
+
     if (!saved.hasAttribute('hidden'))
-        saved.toggleAttribute('hidden');
+    {
+        const temp_card = document.querySelectorAll('.saved_card_eff')
+        p=0
+        while(temp_card[p])
+        {
+            temp_card[p].remove();
+            p++;
+        }
+        saved.hidden=true;
+
+    }
     if (!booked.hasAttribute('hidden'))
         booked.toggleAttribute('hidden');
-    liked.toggleAttribute('hidden');
+
+    if(liked.hasAttribute('hidden'))
+    {liked.removeAttribute('hidden');
+        fetch('/event_saved')
+            .then(response => response.json())
+            .then(response => {
+                p=0;
+                while( response[p])
+                {
+                    const card$p = saved_card.cloneNode(true);
+                    Event_name= card$p.querySelector('.descri')
+
+                    Event_name.innerHTML =response[p].name
+
+
+                    card$p.setAttribute('class','saved_card_eff' )
+                    console.log(card$p);
+                    card$p.removeAttribute('hidden')
+                    liked.appendChild(card$p);
+                    p+=1;
+                }
+
+
+            })
+            .catch(error => {
+
+                    console.error('Error:', error);
+                }
+            );
+    }
+    else
+    {
+        const temp_card = document.querySelectorAll('.saved_card_eff')
+        p=0
+        while(temp_card[p])
+        {
+            temp_card[p].remove();
+            p++;
+        }
+        liked.hidden=true;
+    }
+
+
 });
+
+
+
+
+
+
+
+
+
 
 booked_bouton.addEventListener('click', (event) => {
     if (!liked.hasAttribute('hidden'))
@@ -34,6 +158,17 @@ booked_bouton.addEventListener('click', (event) => {
         saved.toggleAttribute('hidden');
     booked.toggleAttribute('hidden');
 });
+
+
+
+
+
+
+
+
+
+
+
 
 var type = new Typed(".textlin", {
     strings: ["Check  ", "Book  ", "Like  "],
--- src/Controller/EventUserController.php
+++ src/Controller/EventUserController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use function Webmozart\Assert\Tests\StaticAnalysis\string;
-use Symfony\Component\Security\Core\Security;
+
 class EventUserController extends AbstractController
 {
 
@@ -23,8 +23,42 @@ class EventUserController extends AbstractController
         }
 
         $userIsFound = $entityManager->getRepository(User::class)->findOneById($session->get('id'));
+
         return $this->render('event_user/index.html.twig', [
             'user'=>$userIsFound
         ]);
     }
+
+
+
+
+
+
+    #[Route('/event_saved', name: 'event_saved')]
+    public function liked(EntityManagerInterface $entityManager, SessionInterface $session): JsonResponse
+    {
+        $userIsFound = $entityManager->getRepository(User::class)->findOneById($session->get('id'));
+
+        if (!$userIsFound) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $events = $userIsFound->getEvents();
+
+
+        $responseData = [];
+        foreach ($events as $event) {
+            $responseData[] = [
+                'name' => $event->getName(),
+
+            ];
+        }
+
+        return new JsonResponse($responseData);
+    }
+
+
+
+
+
 }
